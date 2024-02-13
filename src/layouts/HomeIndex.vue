1f79b19171bcde1f011f832d2ffd9f1921ee9556
Tiezheng Yuan <tiezhengyuan@hotmail.com>
2024-02-13 11:58:43 -0500

    boostrap-vue components: home

---
+++ src/layouts/HomeIndex.vue
@@ -0,0 +1,14 @@
+<template>
+
+<div class="text-center">
+  <b-jumbotron header="eRNA" lead="v2. 2022-2024">
+    <p>Bioinformatics for RNA-Seq data analysis</p>
+    <b-button href="#"
+       variant="primary" class="font-weight-bold"
+    >LogIn</b-button>
+    <b-button  href="#" variant="secondary"
+    >LogIn with Google</b-button>
+  </b-jumbotron>
+</div>        
+
+</template>
---
+++ src/layouts/HomeLogin.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.food"
+          :options="foods"
+          required
+        ></b-form-select>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">form</pre>
+    </b-card>
+  </div>
+</template>
